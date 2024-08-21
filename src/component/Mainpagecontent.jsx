b860cefbb8efd20f5196ccae35c8ecb653893524
Skinny001 <bamigboyetemitope84@gmail.com>
2024-08-22 00:29:53 +0100

    my first commit

---
+++ src/component/Mainpagecontent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../App.css"
+import { useState } from 'react'
+
+const Mainpagecontent = (props) => {
+  const[count, setcount] = useState(0)
+
+  let increment=()=>{
+    setcount(count + 1)
+    
+  };
+  return (
+        <div className="card">
+            <img src={props.img} alt="The redemption" style={{width:"100%"}} />
+                
+            <h1>{props.movieName}</h1>
+            <p className="price">#5,000</p>
+            <p>{props.movieAttr}</p>
+            <p><button onClick={increment} className="btn btn-lg"> like <i className="fas fa-heart"></i> {count}</button></p>
+          </div> 
+  )
+}
+
+export default Mainpagecontent
